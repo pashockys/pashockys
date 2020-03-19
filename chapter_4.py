53e31fd247103113fa10f9aef917e7ce5a50af32
pashockys <pashockys92@mail.ru>
2020-03-19 23:37:00 +0700

    learning list+dict

--- chapter_4.py
+++ chapter_4.py
@@ -17,4 +17,26 @@ print(string_2.strip(), "--cool method for avoiding spec symbols from the begini
 string_3 = "hui, zhopa, pizda"
 print(string_3.split(), "-- splitting your string")
 print(string_3.split("z"), "-- splitting your string with some symbol, that you choose")
-
+string_4, string_5, string_6 = ["zalupa", "zzzzzzzzzzzalupa", "biba"]
+print("{:15}{:15}{:>15}".format(string_4, string_5, string_6))  # trying to create a table with columns
+print(f"asa:{string_4}")
+print("asa:{}".format(string_4))
+'''
+LIST
+'''
+list_0 = ["hui", "zalupen'", "pizda"]
+print(f"{list_0}")
+list_1 = list(string_4)
+print(f"{list_1}")
+list_1[0] = "p"
+print(f"{list_1}")
+'''
+Dictionary
+'''
+dict_0 = {
+    'name': 'zalupa',
+    'number': 13,
+    'roflan': "Buldyga"
+}
+print(f"my new awesome dict:{dict_0}")
+print(f"{dict_0['roflan']}")  # printing one element of dict
