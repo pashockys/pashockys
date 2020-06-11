fc1fb1786d48e3c02c6d4500687b1655ab99340e
pavel.kolesnikov <pavel.kolesnikov@eltex.loc>
2020-06-11 11:09:39 +0700

    sdfdf

--- chapter_15.py
+++ chapter_15.py
@@ -8,29 +8,27 @@ def get_ip_from_cfg(filename):
             '/home/pashockys/progi_python/pyneng-examples-exercises//exercises_for_test/15_module_re/'+filename):
         path = ('/home/pashockys/progi_python/pyneng-examples-exercises//exercises_for_test/15_module_re/'+filename)
     else:
-        path = '/home/pashockys/Scripts/Natasha/pyneng-examples-exercises//exercises_for_test/15_module_re/'+filename
+        path = '/home/pashockys/Scripts/Natasha/pyneng-examples-exercises/exercises/15_module_re/'+filename
     with open(path, 'r')as f:
         match = re.findall('ip address (\w+\.\w+\.\w+\.\w+) (\w+\.\w+\.\w+\.\w+)', f.read())
-        print(match)
+        return match
 
 
 def parse_sh_ip_int_br(filename):
+    out_list = []
     if os.path.exists(
             '/home/pashockys/progi_python/pyneng-examples-exercises//exercises_for_test/15_module_re/'+filename):
         path = ('/home/pashockys/progi_python/pyneng-examples-exercises//exercises_for_test/15_module_re/'+filename)
     else:
-        path = '/home/pashockys/Scripts/Natasha/pyneng-examples-exercises//exercises_for_test/15_module_re/'+filename
+        path = '/home/pashockys/Scripts/Natasha/pyneng-examples-exercises/exercises/15_module_re/'+filename
     with open(path, 'r')as f:
-        # match = re.finditer('(\S+) +((\w+\.\w+\.\w+\.\w+)|unassigned) +\S+ +\S+ (up|down|administratively down) +(\S+)', f.read())
-        # match = re.finditer('(\S+) +(\S+) +\S+ +\S+ (up|down|administratively down) +(\S+)', f.read())
-        # match = re.finditer('(\S+) +(\S+) +\S+ +\S+ +(administratively down) +(\S+)', f.read())
-        # match = re.finditer('(administratively down) +(\S+)', f.read())
+        match = re.finditer('(\S+) +(\w+\.\w+\.\w+\.\w+|unassigned) +\S+ +\S+ +(up|down|administratively down) +(\S+)', f.read())
     for i in match:
-        print(i.groups())
-
-# get_ip_from_cfg('config_r1.txt')
-parse_sh_ip_int_br('sh_ip_int_br.txt')
+        out_list.append(i.groups())
+    return out_list
 
 
+print(get_ip_from_cfg('config_r1.txt'))
+print(parse_sh_ip_int_br('sh_ip_int_br.txt'))
 
-#FastEthernet0/3            unassigned      YES unset  administratively down down
+
