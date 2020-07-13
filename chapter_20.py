fb17dfe8d812aef5dba8b16c3a58c3ea52c7f5fa
pavel.kolesnikov <pavel.kolesnikov@eltex.loc>
2020-07-13 18:14:39 +0700

    asdasd

--- chapter_20.py
+++ chapter_20.py
@@ -4,18 +4,26 @@ import logging
 import netmiko
 import yaml
 import os
+from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
 
 if os.path.exists('/home/pashockys/progi_python/pyneng-examples-exercises/exercises/20_concurrent_connections/'):
     path = '/home/pashockys/progi_python/pyneng-examples-exercises/exercises/20_concurrent_connections/'
 else:
     path = '/home/pashockys/Scripts/Natasha/pyneng-examples-exercises/exercises/20_concurrent_connections/'
 
+'''
+examples
+'''
+'''
+logging module
+'''
 # эта строка указывает, что лог-сообщения paramiko будут выводиться
 # только если они уровня WARNING и выше
 logging.getLogger("paramiko").setLevel(logging.WARNING)
 
 logging.basicConfig(
-    format = '%(threadName)s %(name)s %(levelname)s: %(message)s',
+    format='%(threadName)s %(name)s %(levelname)s: %(message)s',
     level=logging.INFO)
 
 
@@ -24,15 +32,28 @@ def send_show(device, show):
     received_msg = '<=== {} Received:   {}'
     ip = device["ip"]
     logging.info(start_msg.format(datetime.now().time(), ip))
+    start_time = datetime.now()
+    try:
+        with netmiko.ConnectHandler(**device) as ssh:
+            ssh.enable()
+            result = ssh.send_command(show)
+            logging.info(received_msg.format(datetime.now().time(), ip))
+            print(f'executing command "{show}" took {datetime.now()-start_time} on device {ip}')
+            return result
+    except netmiko.ssh_exception.NetmikoAuthenticationException:
+        print('oooooops')
 
-    with netmiko.ConnectHandler(**device) as ssh:
-        ssh.enable()
-        result =  ssh.send_command(show)
-        logging.info(received_msg.format(datetime.now().time(), ip))
-    return result
+def execute_command(devices, command):
+    output = {}
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        result = executor.map(send_show, devices, repeat(command))
+        for res, dev in zip(result, devices):
+            output[dev['ip']] = res
+    return output
+
 
 if __name__ == "__main__":
     with open(path+'my_devices.yaml') as f:
         devices = yaml.safe_load(f)
-    for dev in devices:
-        print(send_show(dev, 'sh clock'))
+    for items, values in execute_command(devices, 'sh date').items():
+        print(items, values)
