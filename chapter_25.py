37cd05e102510535dcd6a9f13e7838a79f63facf
pashockys <pashockys92@mail.ru>
2020-08-26 23:37:51 +0700

    as

--- chapter_25.py
+++ chapter_25.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import telnetlib
 '''
-Task 25.1
+Task 25.1a
 '''
 def transform_topology(new_dict):
     repeated_dict = {}
@@ -23,20 +23,37 @@ topology_example = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
                     ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'),
                     ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'),
                     ('SW1', 'Eth0/3'): ('R3', 'Eth0/0')}
-class Topology:
-    def __init__(self, new_dict):
-        repeated_dict = {}
-        for key, value in new_dict.items():
-            for key2, value2 in new_dict.items():
-                if key == value2 and value == key2:
-                    if key2 == repeated_dict.get(value2):
-                        continue
-                    else:
-                        repeated_dict[key2] = value2
-        result = {k: v for k, v in new_dict.items() if k not in repeated_dict}
-        self.topology = result
+# class Topology:
+#     def __init__(self, new_dict):
+#         repeated_dict = {}
+#         for key, value in new_dict.items():
+#             for key2, value2 in new_dict.items():
+#                 if key == value2 and value == key2:
+#                     if key2 == repeated_dict.get(value2):
+#                         continue
+#                     else:
+#                         repeated_dict[key2] = value2
+#         result = {k: v for k, v in new_dict.items() if k not in repeated_dict}
+#         self.topology = result
 
-
+#     class Topology:
+#         def __init__(self, topology_dict):
+#             self.topology = self._normalize(topology_dict)
+#
+#     def _normalize(self, new_dict):
+#         repeated_dict = {}
+#             for key, value in new_dict.items():
+#                 for key2, value2 in new_dict.items():
+#                     if key == value2 and value == key2:
+#                         if key2 == repeated_dict.get(value2):
+#                             continue
+#                         else:
+#                             repeated_dict[key2] = value2
+#         result = {k: v for k, v in new_dict.items() if k not in repeated_dict}
+#         return result
+#
+#
+#
 # top = Topology(topology_example)
 # print(top.topology)
 
@@ -52,26 +69,61 @@ r1_params = {
 
 class CiscoTelnet:
     def __init__(self, dict):
+        enable_pass = dict['secret'].encode('ascii')
+        password = dict['password'].encode('ascii')
+        username = dict['username'].encode('ascii')
         print('Connection to device {}'.format(dict['ip']))
-        with telnetlib.Telnet(dict['ip']) as self.t:
-            self.t.read_until(b'Username:')
-            self.t.write(b"dict['username']" + b'\n')
-            self.t.read_until(b'Password:')
-            self.t.write(b"dict['password']" + b'\n')
-            self.t.write(b'enable\n')
-            self.t.read_until(b'Password:')
-            self.t.write(b"dict['secret']" + b'\n')
-            self.t.write(b'terminal length 0\n')
+        self.t = telnetlib.Telnet(dict['ip'])
+        self.t.read_until(b'Username:')
+        self.t.write(username + b'\n')
+        self.t.read_until(b'Password:')
+        self.t.write(password + b'\n')
+        self.t.write(b'enable\n')
+        self.t.read_until(b'Password:')
+        self.t.write(enable_pass + b'\n')
+        # self.t.read_until(b'R4#')
+            # self.t.write(b'sh ip int br\n')
+            # print(self.t.read_until(b'R4#').decode('ascii'))
 
     def send_show_command(self, command):
         command = command.encode('ascii')
-        print(command)
+        self.t.read_until(b'R4#')
         self.t.write(command + b'\n')
-        return self.t.read_very_eager().decode('ascii')
+        print(self.t.read_until(b'R4#').decode('ascii'))
 
 
 
 
 
 r1 = CiscoTelnet(r1_params)
-print(r1.send_show_command('sh ip int br'))
+r1.send_show_command('sh ip int br')
+
+
+
+
+
+
+
+
+
+#
+# class CiscoTelnet:
+#     def __init__(self, dict):
+#         enable_pass = dict['secret'].encode('ascii')
+#         password = dict['password'].encode('ascii')
+#         username = dict['username'].encode('ascii')
+#         print('Connection to device {}'.format(dict['ip']))
+#         t = telnetlib.Telnet('192.168.100.1')
+#         with telnetlib.Telnet(dict['ip']) as self.t:
+#             self.t.read_until(b'Username:')
+#             self.t.write(username + b'\n')
+#             self.t.read_until(b'Password:')
+#             self.t.write(password + b'\n')
+#             self.t.write(b'enable\n')
+#             self.t.read_until(b'Password:')
+#             self.t.write(enable_pass + b'\n')
+            # self.t.read_until(b'R4#')
+            # self.t.write(b'sh ip int br\n')
+            # print(self.t.read_until(b'R4#').decode('ascii'))
+
+
