7058b3630dd409c9731af36b9ce0dba4fd99d113
pashockys <pashockys92@mail.ru>
2020-09-24 22:37:29 +0700

    sf

--- adv_chapter_6_closure.py
+++ adv_chapter_6_closure.py
@@ -39,13 +39,69 @@ def netmiko_ssh(**params_dict):
 task 6.2
 '''
 
-def count_total(num):
-    sum1 = num
-    def inner():
+def count_total():
+    sum1 = 0
+    def inner(num):
         nonlocal sum1
         sum1 = num + sum1
-        return sum1
+        print(f'sum {sum1}')
     return inner
 
-print(f'{count_total(15)()}')
-print(f'{count_total(20)()}')
+# count = count_total()
+# count(10)
+# count(20)
+# count(30)
+
+'''
+task 6.2a
+'''
+
+
+
+def count_total():
+    sum1 = 0
+    def inner():
+        def summ(num):
+            nonlocal sum1
+            sum1 = num + sum1
+            print(f'sum {sum1}')
+        inner.buy = summ
+    return inner
+
+# count = count_total()
+# count()
+# count.buy(10)
+# count.buy(20)
+# count.buy(30)
+
+'''
+task 6.3
+'''
+
+def listing():
+    list_res = []
+    def inner_f():
+        def add_to_queue(var):
+            list_res.append(var)
+            print(f'you have just added item {var} to queue '
+                  f'now the whole queue looks like this: '
+                  f'{list_res}')
+        def del_from_queue(var):
+            try:
+                list_res.remove(var)
+                print(f'you have just removed item {var} from queue '
+                      f'now the whole queue looks like this: '
+                      f'{list_res}')
+            except ValueError:
+                print(f" you are trying to remove {var} "
+                      f"but we don't have this item in our list")
+        inner_f.put = add_to_queue
+        inner_f.get = del_from_queue
+    return inner_f
+
+nikita = listing()
+nikita()
+nikita.put(20)
+nikita.get(20)
+nikita.get(30)
+nikita.put('sds')
